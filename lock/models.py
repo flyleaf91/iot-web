2ad22c2eb0bb2be9f0d3901261d8ad86a51dda6c
Jay <smile665@gmail.com>
2015-05-18 16:55:17 +0800

    add LockCmd model/serializers

--- lock/models.py
+++ lock/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class LockInfo(models.Model):
     device_id = models.CharField(max_length=128, db_index=True)
     event_type = models.IntegerField()
@@ -10,3 +10,14 @@ class LockInfo(models.Model):
 
     class Meta:
         db_table = 'lock_info'
+
+
+class LockCmd(models.Model):
+    device_id = models.CharField(max_length=128, db_index=True)
+    command = models.CharField(max_length=128)
+    status = models.CharField(max_length=16)
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'lock_cmd'
--- lock/serializers.py
+++ lock/serializers.py
@@ -5,3 +5,8 @@ import lock.models as models
 class LockInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.LockInfo
+
+
+class LockCmdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.LockCmd
