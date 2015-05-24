ca1209991e24ec045b202f23883c8656b2e74636
Jay <smile665@gmail.com>
2015-05-24 16:33:37 +0800

    make the lockcmd job run standalone

--- jobs/lockcmd_job.py
+++ jobs/lockcmd_job.py
@@ -4,6 +4,12 @@ Created on May 24, 2015
 @author: Jay
 '''
 import django
+import os
+import sys
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'iot.settings'
 from lock.lock_cmd import cmd_timeout
 
 timeout = 60
